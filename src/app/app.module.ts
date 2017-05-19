25bc269674380536e1889bafb575c7a2b88211ab
mazhekin <mazhekin@gmail.com>
2017-05-19 11:26:48 +0200

    try material buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MdButtonModule, MdCheckboxModule} from '@angular/material';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +15,12 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+
+    BrowserAnimationsModule,
+
+    MdButtonModule,
+    MdCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
